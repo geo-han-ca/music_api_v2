eb73b1ba951aad75a29ef121d50c2ac38492749f
George Hanson <geo.han.ca@gmail.com>
2023-03-08 13:18:05 -0800

    Update settings.py

--- app/mymedia/settings.py
+++ app/mymedia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7f#-*nfj&@+v*v#z%uqa=2kmh)u2$)rojux*)3tz@u&i!7r-f-'
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
